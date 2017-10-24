563e177869e3bdff66e350c75ccdf6b0c810af26
Bogdan Sapotnitsky <godgivensap@gmail.com>
2017-10-24 19:12:34 +0300

    Update scraping_cars.py

--- scraping_cars.py
+++ scraping_cars.py
@@ -7,7 +7,7 @@ import xlsxwriter
 website = 'http://www.autotrader.co.uk'
 make = "Audi"
 records = []
-
+# go through all pages of the website
 for i in range(1, 49):
 	url = 'http://www.autotrader.co.uk/car-search?sort=sponsored&postcode=w1a0ax&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&make=AUDI&year-from=2013&maximum-mileage=40000&seller-type=private&page={}'.format(i)
 	r = uReq(url)
@@ -15,11 +15,10 @@ for i in range(1, 49):
 	r.close()
 	page_soup = soup(page_html, 'html.parser')
 	containers = page_soup.find_all('li', attrs={'class':'search-page__result'})
-
+	# go through all car items of the page
 	for container in containers:
 		time.sleep(2)
 		item_adress = container.find('a', attrs={'class':'listing-fpa-link'})["href"]
-
 		item_r = uReq(website + item_adress)
 		item_page_html = item_r.read()
 		item_r.close()
@@ -31,7 +30,7 @@ for i in range(1, 49):
 		records.append((make, model, price, mileage, phone))
 
 	print("page " + str(i) + " from 48 done")
-
+# put data to Excelsheet by Pandas
 df = pd.DataFrame(records, columns=['Make', 'Model', 'Price', 'Mileage', 'Telephone Number'])  
 writer = pd.ExcelWriter('Audi.xlsx', engine='xlsxwriter')
 df.to_excel(writer, index=False, sheet_name='Sheet1') 
